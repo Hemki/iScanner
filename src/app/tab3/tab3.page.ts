502043b975430a274197b1c6ae8f523e4bb3059a
Jonas-418 <jhemkendreis@gmx.de>
2023-08-05 11:52:57 +0200

    Implement RSSI Filter functionality

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,17 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { StorageService } from '../services/shared/storage/storage.service';
+
+// Storage
+// RSSI {
+//  enabled: true // Filter enabled?
+//  value: -80
+// }
+
+interface RSSIFilter {
+  enabled: boolean,
+  value: number
+}
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit{
 
-  constructor(private router: Router) {}
+  rssiFilter: RSSIFilter = {
+    enabled: false,
+    value: 80,
+  };
+
+  constructor(private router: Router, private storage: StorageService) {}
+
+  async ngOnInit(): Promise<void> {
+    const storedRssiFilter = await this.storage.get("rssi");
+    if (storedRssiFilter) {
+      this.rssiFilter = storedRssiFilter;
+    }
+  }
 
   goToUuidsPage() {
     this.router.navigateByUrl('/tabs/tab3/uuids');
+  }
+
+  pinFormatter(value: number) {
+    return `-${value}`;
+  }
+
+  async updateRssiFilter(){
+    await this.storage.set("rssi",this.rssiFilter);
   }
 
 }
