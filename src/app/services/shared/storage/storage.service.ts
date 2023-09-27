1f4eb7fb02527e6d528db285acdd9aac369708b3
Jonas-wtec <jhemkendreis@wtec.io>
2023-09-27 17:38:13 +0200

    Remove console log

--- src/app/services/shared/storage/storage.service.ts
+++ src/app/services/shared/storage/storage.service.ts
@@ -28,7 +28,6 @@ export class StorageService {
 
     if ((result === null && key === "uuids") || (result instanceof Set && result.size === 0)) {
       // Set initial UUID for demo version
-      console.log("IN HERE");
       return new Set(["b7e18b02-5ca0-11ee-8c99-0242ac120002"]);
     }
 
